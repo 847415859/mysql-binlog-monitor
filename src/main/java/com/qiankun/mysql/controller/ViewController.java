a0db4965302ff18fb7e4c69417f12678748c165f
tiankun <847415859@qq.com>
2023-11-09 18:59:26 +0800

    强制修改返回值为 Content-Type:application/json 模式，避免出现@RestController 不生效的情况

--- src/main/java/com/qiankun/mysql/controller/ViewController.java
+++ src/main/java/com/qiankun/mysql/controller/ViewController.java
@@ -8,20 +8,27 @@ package com.qiankun.mysql.controller;
 import com.qiankun.mysql.Replicator;
 import com.qiankun.mysql.dest.AbstractProcessor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "/mysql/monitor")
-public class VIewController {
+@RequestMapping(value = "/mysql/monitor",produces = {MediaType.APPLICATION_JSON_VALUE})
+public class ViewController {
 
     @GetMapping("/viewChange")
-    public Object viewChange(String modelId){
+    public Map viewChange(String modelId){
         if(StringUtils.isEmpty(modelId) || Replicator.replicator == null || Replicator.replicator.getProcessor() == null){
             return null;
         }
         AbstractProcessor processor = Replicator.replicator.getProcessor();
-        return processor.viewChange(modelId);
+        Map<String,Object> result = new HashMap<>();
+        result.put("data",processor.viewChange(modelId));
+        return result;
     }
 }
